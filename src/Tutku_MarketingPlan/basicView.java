5625c1b884b207fec4ec522cdcdffb76e9ccd581
Emre_Cydeo <emre.ketenci34@gmail.com>
2023-05-18 01:52:09 -0400

    Overview about selling tutku online has been pushed

---
+++ src/Tutku_MarketingPlan/basicView.java
@@ -0,0 +1,18 @@
+package Tutku_MarketingPlan;
+
+public class basicView {
+    public static void main(String[] args) {
+
+        System.out.println("With how many products we will launch?");
+        System.out.println("\n\t20 products, one of each product. ");
+        System.out.println("What is the approximate quantity per item? ");
+        System.out.println("\n\tif thermals, tShirts or athletes, it is 2 of a color 5 of a size and total of 10 each" +
+                " there are 9 kinds of tops for men total of 90 pieces ");
+        System.out.println("\n\t4 different types of boxers at 30 each at 120 pieces");
+
+        System.out.println("Apply all to the women, 210 pieces for men and 210 pieces for women could be listed.");
+
+        String result = "";
+
+    }
+}
